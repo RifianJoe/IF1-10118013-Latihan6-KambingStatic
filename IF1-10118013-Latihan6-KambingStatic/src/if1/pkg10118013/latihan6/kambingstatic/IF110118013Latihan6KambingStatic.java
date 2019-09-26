562ae98e5db5ad5909c5458591dae43f0b0fb4dd
RifianJoe <55788355+RifianJoe@users.noreply.github.com>
2019-09-26 15:07:55 +0700

    Add files via upload

---
+++ IF1-10118013-Latihan6-KambingStatic/src/if1/pkg10118013/latihan6/kambingstatic/IF110118013Latihan6KambingStatic.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118013.latihan6.kambingstatic;
+
+/**
+ *
+ * @author Rifian
+ * Nama : Rifian Joe Premilenio
+ * Kelas : IF-1
+ * NIM : 10118013
+ * Deskripsi Program : Program ini berisi program untuk menampilkan jenis-jenis tipe data bilangan bulat
+ */
+public class IF110118013Latihan6KambingStatic {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static final String NAMA_KAMBING = "MIDUN";
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        IF110118013Latihan6Mamalia.jumlahKambing = 485000;
+        System.out.println(NAMA_KAMBING + " memiliki kambing sebanyak " + IF110118013Latihan6Mamalia.jumlahKambing);
+    }
+    
+}
---
+++ IF1-10118013-Latihan6-KambingStatic/src/if1/pkg10118013/latihan6/kambingstatic/IF110118013Latihan6Mamalia.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118013.latihan6.kambingstatic;
+
+/**
+ *
+ * @author Rifian
+ * Nama : Rifian Joe Premilenio
+ * Kelas : IF-1
+ * NIM : 10118013
+ * Deskripsi Program : Program ini berisi program untuk menampilkan jenis-jenis tipe data bilangan bulat
+ */
+public class IF110118013Latihan6Mamalia {
+    public static int jumlahKambing;
+}
